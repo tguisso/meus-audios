397415e4c092ed5cecb98dc57dd5a596012fad68
tatiane <tati.guisso@gmail.com>
2024-06-13 21:47:41 -0300

    first commit

---
+++ src/main/java/com/meus_audios/MeusAudiosApplication.java
@@ -0,0 +1,13 @@
+package com.meus_audios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeusAudiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeusAudiosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meus_audios/MeusAudiosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meus_audios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeusAudiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
