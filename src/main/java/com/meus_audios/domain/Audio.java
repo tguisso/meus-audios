cdd753ee3763d4fd43d501b3c821cdae9f80b459
tatiane <tati.guisso@gmail.com>
2024-06-13 22:22:30 -0300

    implementando audio domain

---
+++ src/main/java/com/meus_audios/domain/Audio.java
@@ -0,0 +1,21 @@
+package com.meus_audios.domain;
+
+import lombok.Getter;
+
+@Getter
+public class Audio {
+	
+	private String titulo;
+	private int totalReproducoes;
+	private int totalCurtidas;
+	private int classificacao;
+	
+	public void curte() {
+		
+	}
+	
+	public void reproduz() {
+		
+	}
+
+}
